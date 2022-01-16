4c314c647323cca02c1b30a69bb69566d7d54bd6
TS93 <tschram93@yahoo.com>
2022-01-15 21:21:37 -0500

    utilized exoress and set static folder

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const uuid = require('uuid');
+
+
+app.use(express.json());
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+// set static folder to public
+app.use(express.static('public/'));
