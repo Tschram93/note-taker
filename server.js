287943d2b09be55f8afc65f5c8c5b1216473d7d2
TS93 <tschram93@yahoo.com>
2022-01-15 21:20:36 -0500

    added packages required

---
+++ server.js
@@ -0,0 +1,7 @@
+// require packages
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const uuid = require('uuid');
